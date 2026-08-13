614f39d230f81bebf65c05b7a9c1cbd52529418e
devpl <devpl@fluidsystem.fr>
2026-08-13 18:07:46 +0200

    Ajout de la configuration de kaliok

---
+++ src/kaliok/config.py
@@ -0,0 +1,2 @@
+APP_NAME = "kaliok V2"
+APP_VERSION = "0.1.0"
--- src/kaliok/core.py
+++ src/kaliok/core.py
@@ -1,7 +1,10 @@
+from .config import APP_NAME, APP_VERSION
+
+
 class Kaliok:
     def __init__(self):
-        self.name = "Kaliok V2"
-        self.version = "0.1.0"
+        self.name = APP_NAME
+        self.version = APP_VERSION
 
     def start(self):
         return f"{self.name} {self.version} est prêt."
